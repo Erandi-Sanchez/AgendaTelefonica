ffdf8bb5308033e77af4b105214a77588830d9d4
Erandi-Sanchez <sanchezgarciaerandi@gmail.com>
2025-03-25 14:54:44 -0600

    Tamaño de agenda

--- ErandiSanchez/src/main/java/org/genetation/util/Agenda.java
+++ ErandiSanchez/src/main/java/org/genetation/util/Agenda.java
@@ -1,4 +1,27 @@
 package org.genetation.util;
+import java.util.ArrayList; //ArrayList: Lista dinámica, almacenar los contactos.
+import java.util.Collections; //Collections: Proporciona métodos estáticos
+import java.util.List; //List: Interfaz que define las operaciones básicas de una lista
+
+
 
 public class Agenda {
+    //Atributos de la clase
+    private List<Contacto> contactos; //Lista contactos que almacena objetos de tipo contacto
+    private int tamanoMax; //Tamño máximo de la lista
+
+    //Metodo constructor de tamaño
+    public Agenda (int tamanoMax){
+        this.contactos = new ArrayList<>(); //Inicializa la lista como una nueva instancia de Arraylist
+        this.tamanoMax = tamanoMax; //Asigna el valor del parametro al atributo
+    }
+    //Llamado al metodo constructor (se puede cambiar el tamaño)
+    public Agenda(){
+        this(10);
+    }
+
+    
+
+
+
 }
